2b7ebda53912ecdcea9f0903a10a8adb69008374
Bailey P. Costello <bcostello@mri-inc.net>
2020-10-17 01:11:27 -0400

    Player Turns

---
+++ Client/src/main/java/com/tictactoe/server/game/Move.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tictactoe.server.game;
+
+/**
+ * Authored by: Bailey Costello
+ * 2020
+ */
+public class Move {
+
+    public int playerID, tileID;
+ 
+    
+}
